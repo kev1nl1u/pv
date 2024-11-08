eeaa3f2f469a0c72afbe919b2e5728bb3c7c7c89
Kevin <kevliujudi@gmail.com>
2024-11-08 12:17:59 +0100

    Add group order

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-/* shared todo list
-
+/* 
 psql
 
-basic db: (temp demo)
+todo db:
 CREATE TABLE todo (
 	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	id SERIAL PRIMARY KEY,
@@ -13,6 +12,13 @@ CREATE TABLE todo (
 	doneby VARCHAR DEFAULT NULL
 );
 
+order db:
+CREATE TABLE orders (
+	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	id SERIAL PRIMARY KEY,
+	cibo VARCHAR,
+	foruser VARCHAR
+);
 */
 
 const express = require('express');
@@ -43,7 +49,16 @@ app.get('/', (req, res) => {
 app.get('/todo', async (req, res) => {
 	try {
 		const results = await pool.query("SELECT * FROM todo ORDER BY doneby IS NOT NULL, time DESC");
-		res.render('todo.ejs', { lista: results.rows, active: 'lista-btn'});
+		res.render('todo.ejs', { lista: results.rows });
+	} catch (error) {
+		console.error(error.message);
+	}
+});
+
+app.get('/order', async (req, res) => {
+	try {
+		const results = await pool.query("SELECT * FROM orders ORDER BY id ASC");
+		res.render('order.ejs', { lista: results.rows });
 	} catch (error) {
 		console.error(error.message);
 	}
@@ -51,7 +66,9 @@ app.get('/todo', async (req, res) => {
 
 // api
 
-app.post('/api/add', async (req, res) => {
+// todo
+
+app.post('/api/todo/add', async (req, res) => {
 	try {
 		const { type, title, description, user} = req.body;
 		const newTodo = await pool.query("INSERT INTO todo (type, title, description, foruser) VALUES($1, $2, $3, $4) RETURNING *", [type, title, description, user]);
@@ -61,7 +78,7 @@ app.post('/api/add', async (req, res) => {
 	}
 });
 
-app.get('/api/get/:id', async (req, res) => {
+app.get('/api/todo/get/:id', async (req, res) => {
 	try {
 		const { id } = req.params;
 		const todo = await pool.query("SELECT * FROM todo WHERE id = $1", [id]);
@@ -71,7 +88,7 @@ app.get('/api/get/:id', async (req, res) => {
 	}
 });
 
-app.post('/api/update/:id', async (req, res) => {
+app.post('/api/todo/update/:id', async (req, res) => {
 	try {
 		const { id } = req.params;
 		const { type, title, description, user } = req.body;
@@ -82,7 +99,7 @@ app.post('/api/update/:id', async (req, res) => {
 	}
 });
 
-app.get('/api/delete/:id', async (req, res) => {
+app.get('/api/todo/delete/:id', async (req, res) => {
 	try {
 		const { id } = req.params;
 		const deletedTodo = await pool.query("DELETE FROM todo WHERE id = $1", [id]);
@@ -92,7 +109,7 @@ app.get('/api/delete/:id', async (req, res) => {
 	}
 });
 
-app.post('/api/doneby/:id', async (req, res) => {
+app.post('/api/todo/doneby/:id', async (req, res) => {
 	try {
 		const { id } = req.params;
 		const { doneby } = req.body;
@@ -103,7 +120,7 @@ app.post('/api/doneby/:id', async (req, res) => {
 	}
 });
 
-app.get('/api/undone/:id', async (req, res) => {
+app.get('/api/todo/undone/:id', async (req, res) => {
 	try {
 		const { id } = req.params;
 		const undoneTodo = await pool.query("UPDATE todo SET doneby = NULL WHERE id = $1", [id]);
@@ -113,6 +130,73 @@ app.get('/api/undone/:id', async (req, res) => {
 	}
 });
 
+// order
+
+app.post('/api/order/add', async (req, res) => {
+	try {
+		const { cibo, user} = req.body;
+		const newOrder = await pool.query("INSERT INTO orders (cibo, foruser) VALUES($1, $2) RETURNING *", [cibo, user]);
+		res.redirect('/order');
+	} catch (error) {
+		console.error(error.message);
+	}
+});
+
+app.get('/api/order/get/:id', async (req, res) => {
+	try {
+		// use id and foruser to get order
+		const { id } = req.params;
+		const order = await pool.query("SELECT * FROM orders WHERE id = $1", [id]);
+		res.json(order.rows[0]);
+	} catch (error) {
+		console.error(error.message);
+	}
+});
+
+app.post('/api/order/update/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { cibo, user } = req.body;
+		// user can be null when update default order
+		const updatedOrder = await pool.query(
+			"UPDATE orders SET cibo = $1, foruser = COALESCE($2, foruser) WHERE id = $3 RETURNING *",
+			[cibo, user, id]
+		);
+
+		if (updatedOrder.rowCount === 0) {
+			return res.status(404).json({ message: "Order not found" });
+		}
+
+		res.json(updatedOrder.rows[0]);
+	} catch (error) {
+		console.error(error.message);
+	}
+});
+
+app.get('/api/order/delete/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		if(id > 0 && id < 9){
+			res.redirect('/order');
+			return;
+		}
+		const deletedOrder = await pool.query("DELETE FROM orders WHERE id = $1", [id]);
+		res.redirect('/order');
+	} catch (error) {
+		console.error(error.message);
+	}
+});
+
+app.get('/api/order/azzera', async (req, res) => {
+	try {
+		const deletedOrder = await pool.query("DELETE FROM orders WHERE id > 8");
+		// set null orders
+		await pool.query("UPDATE orders SET cibo = NULL");
+		res.redirect('/order');
+	} catch (error) {
+		console.error(error.message);
+	}
+});
 
 /* set static folder for css etc */
 app.use(express.static(path.join(__dirname, 'public')));
